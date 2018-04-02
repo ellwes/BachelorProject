82a0d82def34fa68559cf464c38d4da8ee40c774
Lamacoder <eric.lewerentz@hotmail.com>
2018-04-02 10:03:25 +0200

    Made quarter reports plot optional

--- runAll.sh
+++ runAll.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-startDate = "2017-01-07" #format: 2017-01-
+#startDate = "2017-01-07" #format: 2017-01-
 
 python sentiment_analysis.py > test.dat 
 
 gnuplot << EOF
+	plot_quarter_data=0
+
 	set xdata time
 	set timefmt "%Y-%m-%d"
-	set xrange["2017-01-07": "2017-01-10"]
-	set terminal png font arial 14 size 800,600
+	set xrange["2013-01-28": "2018-02-28"]
+	set terminal png font arial 14 size 1500,800
 	set output 'plot.png'
-	plot "test.dat" using 1:2 with linespoints title "Sentiments", "test.dat" using 1:3 title "Quater Result"
-
+	if (plot_quarter_data == 1) {
+		plot "test.dat" using 1:2 with linespoints title "Sentiments", "test.dat" using 1:3 title "Quater Result"
+	} else {
+	    plot "test.dat" using 1:2 with linespoints title "Sentiments"
+	}
 EOF
