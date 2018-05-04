1c275ae2cd18f8e59313f59668ea3e01d393e461
Ellinor Westerberg <ell.westerberg@gmail.com>
2018-05-04 10:46:07 +0200

    Output sentiments in csv-file. Created own dir for that

--- sentiment_analysis_short.py
+++ sentiment_analysis_short.py
@@ -9,54 +9,7 @@ start_date = sys.argv[1]
 end_date = sys.argv[2]
 
 file_with_data = "Data/" + sys.argv[3] + ".csv"
-file_with_result = "Results/" + sys.argv[4] + ".txt"
-
-
-#reads the result from a file
-date = ''
-result1 = '?'
-result2 = '?'
-result3 = '?'
-
-results = [] #Holds the results from the quater reports
-descriptor1 = 'Total net sales'
-descriptor2 = 'Net income (loss)'
-descriptor3 = 'Diluted'
-
-def extractNumbers(s):
-    s = re.findall("\(?\d*\.?\d+\)?" , s)[0]
-    if s[0] == '(':
-        s = '-' + s[1:]
-    return s
-
-result_file = open(file_with_result, 'r')
-for line in result_file:
-    if line.rstrip().isdigit():
-        #Add result to resutls-list
-        if result1 != '?' or result2 != '?' or result3 != '?':
-            results.append((date, result1, result2, result3));
-            date = ''
-            result1 = '?'
-            result2 = '?'
-            result3 = '?'
-        #Update date
-        date = line[0:4] + '-' + line[4:6] + '-' + line[6:8] + '_08:00'
-
-    elif line[0] == "'":
-        descriptor = re.findall("('.+')", line)[0][1:-1]
-        thisQuater = extractNumbers(re.findall("('\s*\(?\d*\.?\d+\)?\s)" , line)[0])
-        lastQuater = extractNumbers(re.findall("(\(?\d*\.?\d+\)?\s*\n)" , line)[0])
-        if descriptor == descriptor1:
-            result1 = (float(thisQuater) - float(lastQuater))/float(lastQuater)
-        elif descriptor == descriptor2:
-            result2 = (float(thisQuater) - float(lastQuater))/float(lastQuater)
-        elif descriptor == descriptor3:
-            result3 = (float(thisQuater) - float(lastQuater))/float(lastQuater)
-
-if result1 != '?' or result2 != '?' or result3 != '?':
-    results.append((date, result1, result2, result3))
-
-
+newFilePath = "Sentiments/" + sys.argv[3] + ".csv"
 
 #Reads the date and the comments from file
 sentences = []
@@ -76,15 +29,12 @@ with open(file_with_data, 'rb') as csvfile:
 #Preformes the analysis
 time_and_sentiments = []
 analyzer = SentimentIntensityAnalyzer()
-for sentence in sentences:
-    vs = analyzer.polarity_scores(sentence[1])
-    time_and_sentiments.append( (sentence[0], vs['compound']))
-    printed = False
-    for res in results: #looks for resluts to print that day
-        if sentence[0][0:10] == res[0]:
-            print sentence[0].replace(' ', '_') + ' ' + str(vs['compound']) + res[1] + ' ' + res[2] + ' ' + res[3]
-            printed = True
-            break;
-    if not printed:
-        print sentence[0].replace(' ', '_') + ' ' + str(vs['compound']) + ' ? ? ?' 
-    printed = False
+with open(newFilePath, 'w') as csvfile:
+    fieldnames = ['date', 'sentiment_value']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for sentence in sentences:
+        vs = analyzer.polarity_scores(sentence[1])
+        time_and_sentiments.append( (sentence[0], vs['compound']))
+        writer.writerow({'date': sentence[0].replace(' ', '_'),  'sentiment_value': vs['compound']})
+        print sentence[0].replace(' ', '_') + ' ' + str(vs['compound'])
