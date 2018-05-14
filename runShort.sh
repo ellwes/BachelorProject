4b82da29b36d7c6c7cc26ef0fae786b02195f8e2
Ellinor Westerberg <ell.westerberg@gmail.com>
2018-05-14 11:40:32 +0200

    removed right measurements

--- runShort.sh
+++ runShort.sh
@@ -4,12 +4,14 @@
 #OBS: Only works correctly if the result from one date is used.
 
 #VARIABLES:
-START_DATE="2015-07-22" #"2014-05-06"
-END_DATE="2015-07-26" #"2014-09-10" #format: YYYY-mm-dd
+START_DATE="2016-05-07" #"2014-05-06"
+START_DATE_GRAPH="2016-05-08" #"2014-05-06"
 
-FILE_NAME="SIMPLE_nvidia_2015-07-22_2015-07-26" #The file in which the tweets can be found.
+END_DATE="2016-05-12" #"2014-09-10" #format: YYYY-mm-dd
 
-DAT_FILE_NAME="SIMPLE_nvidia_2015-07-22_2015-07-26" #The name of the dat-file (that will be used to create the graph)
+FILE_NAME="Activision_Blizzard_2018-04-10_VADER" #The file in which the tweets can be found.
+
+DAT_FILE_NAME="acti_2016_Q1" #The name of the dat-file (that will be used to create the graph)
 
 python sentiment_analysis_short.py $START_DATE $END_DATE $FILE_NAME > Data/${DAT_FILE_NAME}.dat
 
@@ -17,14 +19,10 @@ python sentiment_analysis_short.py $START_DATE $END_DATE $FILE_NAME > Data/${DAT
 gnuplot << EOF
 	set xdata time
 	set timefmt "%Y-%m-%d_%H:%M:%S"
-	set xrange["${START_DATE}_00:00:00": "${END_DATE}_00:00:00"]
+	set xrange["${START_DATE_GRAPH}_00:00:00": "${END_DATE}_00:00:00"]
   set xlabel "Time"
   set yrange[-1:1]
   set ylabel "Sentiment score (decimal)"
-  set y2range[-2:2]
-  set y2label "Procentual changes (decimal)"
-  set ytics nomirror
-  set y2tics
 	set terminal png font arial 14 size 1500,800
 	set output 'Plot/$DAT_FILE_NAME.png'
 	plot "Data/${DAT_FILE_NAME}.dat" using 1:2 with linespoints title "Sentiments" axes x1y1
